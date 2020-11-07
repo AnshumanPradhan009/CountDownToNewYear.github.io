8377d59403db8ada5604649462ec676d5142582c
AnshumanPradhan009 <70283200+AnshumanPradhan009@users.noreply.github.com>
2020-11-07 18:54:58 +0530

    See The Count Down

--- script.js
+++ script.js
@@ -11,17 +11,69 @@ function countdown() {
 
   const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-  const days = Math.floor(totalSeconds / 3600 / 24);
-  const hours = Math.floor(totalSeconds / 3600) % 24;
-  const minutes = Math.floor(totalSeconds / 60) % 60;
-  const seconds = Math.floor(totalSeconds) % 60;
+  let days = Math.floor(totalSeconds / 3600 / 24);
+  let hours = Math.floor(totalSeconds / 3600) % 24;
+  let minutes = Math.floor(totalSeconds / 60) % 60;
+  let seconds = Math.floor(totalSeconds) % 60;
 
   daysEl.innerHTML = days;
   hoursEl.innerHTML = hours;
   minutesEl.innerHTML = minutes;
   secondsEl.innerHTML = seconds;
-}
 
+  if (seconds == 1) {
+    const sec = "Sec";
+    let secs = document.getElementById("Secs");
+    secs.innerText = sec;
+  } else if(seconds == 0){
+    const sec = "Sec";
+    let secs = document.getElementById("Secs");
+    secs.innerText = sec;
+  } else {
+    let secs = document.getElementById("Secs");
+    secs.innerText = "Secs";
+  }
+
+  if (minutes == 1) {
+    const min = "Min";
+    let mins = document.getElementById("mins");
+    mins.innerText = min;
+  } else if(minutes == 0){
+    const min = "Min";
+    let mins = document.getElementById("mins");
+    mins.innerText = min;
+  } else {
+    let mins = document.getElementById("mins");
+    mins.innerText = "Mins";
+  }
+
+  if (hours == 1) {
+    const hour = "Hour";
+    let hours2 = document.getElementById("hours2");
+    hours2.innerText = hour;
+  } else if(hours == 0){
+    const hour = "Hour";
+    let hours2 = document.getElementById("hours2");
+    hours2.innerText = hour;
+  } else {
+    let hours2 = document.getElementById("hours2");
+    hours2.innerText = "Hours";
+  }
+
+  if (days == 1) {
+    const days = "Day";
+    let days2 = document.getElementById("days2");
+    days2.innerText = days;
+  } else if(days == 0){
+    const days = "Day";
+    let days2 = document.getElementById("days2");
+    days2.innerText = days;
+  } else {
+    let days2 = document.getElementById("days2");
+    days2.innerText = "Days";
+  }
+     
+}
 
 countdown();
 
